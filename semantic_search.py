e8744d04ce34ffa06c0180ea7160a9a4bb948677
Thinh Nguyen Kim Hung <thinh.nguyen@orientsoftware.com>
2023-07-27 16:00:25 +0700

    adding semantic search

---
+++ semantic_search.py
@@ -0,0 +1,33 @@
+from pyvi.ViTokenizer import tokenize
+import pandas as pd
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+
+vietnamese_model = SentenceTransformer("models/VietnameseSimCSE")
+data = pd.read_excel('data/data.xlsx')
+# option = data.columns.values[0]
+
+
+def get_results_vi(query, vietnamese_model, data):
+    options = data.columns.values
+    index = faiss.read_index('data/data.index')
+    query = tokenize(query.encode('utf-8').decode('utf-8'))
+    top_k_IDs, score = search_vi(vietnamese_model, index, query, 2)
+    results = [data[options[4]][i] for i in top_k_IDs]
+    return {"results": results, "score": float(score)}
+
+def search_vi(model, index, query, top_k):
+    query_vector = model.encode([query])
+    top_k = index.search(query_vector, top_k)
+    top_k_ids = top_k[1].tolist()[0]
+    top_k_ids = list(np.unique(top_k_ids))
+    return top_k_ids, top_k[0][0][0]
+
+
+query = input()
+search = get_results_vi(query, vietnamese_model, data)
+result = ",".join(search.get('results'))
+print(result)
+print(len(result))
